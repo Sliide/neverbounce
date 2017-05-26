587a2d47b1844221163400dd796a178b1365239b
Mike Mollick <mollick2@gmail.com>
2017-05-26 16:46:27 -0400

    Adding support for other error message formats

--- responses.go
+++ responses.go
@@ -1,13 +1,18 @@
 package neverbounce
 
 type AccessTokenResponse struct {
-	AccessToken string `json:"access_token"`
-	Expires     int    `json:"expires"`
+	AccessToken      string `json:"access_token"`
+	Expires          int    `json:"expires"`
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description"`
 }
 
 type VerifyEmailResponse struct {
-	Success       bool   `json:"success"`
-	Result        int    `json:"result"`
-	ResultDetails int    `json:"result_details"`
-	Msg           string `json:"msg"`
+	Success          bool   `json:"success"`
+	Result           int    `json:"result"`
+	ResultDetails    int    `json:"result_details"`
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description"`
+	ErrorMsg         string `json:"error_msg"`
+	Msg              string `json:"msg"`
 }
